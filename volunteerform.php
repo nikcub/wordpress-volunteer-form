28018fb44bd9152db09851c0d631b829040efba1
Nik Cubrilovic <nikcub@gmail.com>
2015-02-15 13:13:22 +1100

    version bump

--- volunteerform.php
+++ volunteerform.php
@@ -3,7 +3,7 @@
 * Plugin Name: Volunteer Form
 * Plugin URL: https://www.nikcub.com/project/volunteerform
 * Description: Plugin that provides a volunteer form as a shortcode
-* Version: 0.1.3
+* Version: 0.1.4
 * Author: Nik
 * Author URI: https://www.nikcub.com/
 */
